2f3235490268e9bf87991b2bfd07bd69eaa3899d
shuvrod564 <shuvrod564@gmail.com>
2024-09-27 12:54:50 +0600

    updation

--- includes/url.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // $base_url = '192.168.1.4/soulistic/';
-    $base_url = 'http://localhost/basetemplate/';
-    // $base_url = 'https://kerala-cruises.embraceindia.in/';
-
-    $site_name="";
-?>
